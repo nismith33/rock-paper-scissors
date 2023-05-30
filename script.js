33e9e5794e335a9ba8cbf8c2e318f916b2216d09
nismith33 <nismith33@gmail.com>
2023-05-30 19:28:31 -0400

    Add getComputerChoice() function to choose attack for computer

---
+++ script.js
@@ -0,0 +1,14 @@
+/*Picks an attack for the computer to use. The attack is
+chosen randomly
+*/
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3);
+    if (choice===0) {
+        return 'rock';
+    } else if (choice===1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
