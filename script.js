9b98c918de6892711415dd62acfcad77bcf6a1f0
nismith33 <nismith33@gmail.com>
2023-05-30 19:50:05 -0400

    Add roundJudgement() function to determine winner of a round

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 chosen randomly
 */
 function getComputerChoice() {
+    //Choice will only ever be 0, 1, or 2
     let choice = Math.floor(Math.random()*3);
     if (choice===0) {
         return 'rock';
@@ -11,4 +12,25 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
+
+
+/*Determines whether the player or the computer wins a given
+round. Also sets the player input to lower-case for ease of
+comparison
+*/
+function roundJudgement(playerChoice, computerChoice) {
+    //standardize input classification
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice===computerChoice) {
+        return "Tie game";
+    } else if (playerChoice==='rock'&& computerChoice==='scissors'
+    ||playerChoice==='scissors'&& computerChoice==='paper'
+    ||playerChoice==='paper'&& computerChoice==='rock') {
+        return "You win!";
+    } else {
+        return "You lose :(";
+    }
+}
+
+
 
