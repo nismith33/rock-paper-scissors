4a8bf5d1eb94dfd586f7a6d5dbe95882419d35cc
nismith33 <nismith33@gmail.com>
2023-06-06 14:04:48 -0400

    Restructure gameplay to trigger on button press instead of occuring in a loop

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let playerScore = 0;
+let computerScore = 0;
+const WIN_SCORE =3;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.className);
+        playGame(button.className);
+    });
+});
+
+function playGame(playerChoice) {
+    const computerChoice = getComputerChoice();
+    switch (roundJudgement(playerChoice, computerChoice)) {
+        case ('tie'):
+            alert('tie');
+        break;
+        case ("win"):
+            alert('win');
+            playerScore += 1;
+        break;
+        case ("loss"):
+            alert('loss');
+            computerScore += 1;
+        break;
+    }
+    updateScore(playerScore,computerScore);
+}
+
 /*Picks an attack for the computer to use. The attack is
 chosen randomly
 */
@@ -12,7 +41,6 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
-
 
 /*Determines whether the player or the computer wins a given
 round. Also sets the player input to lower-case for ease of
@@ -40,36 +68,3 @@ function updateScore(playerScore,computerScore) {
     const computerScoreCard = document.querySelector('.computerScore');
     computerScoreCard.textContent = `Computer Score: ${computerScore}`;
 }
-
-/*Play rounds of RPS until either the computer or player
-gets WIN_SCORE many wins.
-*/
-function playGame(WIN_SCORE=1) {
-    let playerScore = 0;
-    let computerScore = 0;
-    while (playerScore <WIN_SCORE && computerScore<WIN_SCORE) {
-        let playerChoice = prompt("Choose rock, paper, or scissors");
-        switch (roundJudgement(playerChoice, getComputerChoice())) {
-            case ('tie'):
-            break;
-            case ("win"):
-                playerScore += 1;
-            break;
-            case ("loss"):
-                computerScore += 1;
-            break;
-            default:
-                alert("Your input is incomprehensible");
-        }
-        updateScore(playerScore,computerScore);
-    }
-    if (playerScore===WIN_SCORE) {
-        alert("You won!");
-    } 
-    else {
-        alert("You lost :(");
-    }
-}
-
-
-
